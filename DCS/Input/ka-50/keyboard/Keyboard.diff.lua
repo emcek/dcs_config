e28b66548273a768cf676cedaead2891d8cd329c
Michal Plichta <mplichta@gmail.com>
2019-12-24 01:58:00 +0100

    remove duplication of keystroke

---
+++ DCS/Input/ka-50/keyboard/Keyboard.diff.lua
@@ -0,0 +1,16 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3003pnilunilcd100vd1vpnilvunil"] = {
+			["name"] = "Kneeboard current position mark point",
+			["removed"] = {
+				[1] = {
+					["key"] = "K",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
