8bdb553bd8b73bf2f7ca475efd897d1070fe87eb
Michal Plichta <mplichta@gmail.com>
2020-04-21 21:58:35 +0200

    add mdf 1 and 2 files

---
+++ DCS/Input/ground_unit/mouse/Mouse.diff.lua
@@ -0,0 +1,21 @@
+local diff = {
+	["keyDiffs"] = {
+		["d1580pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Lock",
+			["removed"] = {
+				[1] = {
+					["key"] = "MOUSE_BTN5",
+				},
+			},
+		},
+		["d1581pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Target Unlock",
+			["removed"] = {
+				[1] = {
+					["key"] = "MOUSE_BTN4",
+				},
+			},
+		},
+	},
+}
+return diff
